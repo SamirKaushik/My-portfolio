123c65273be477b1a2cb508e6cbd7c2800d491a2
Samir Kaushik <samirkaushik2002@gmail.com>
2022-02-14 18:07:03 +0530

    project.html

--- projects.js
+++ projects.js
@@ -5,8 +5,8 @@ for (var i = 0; i < project.length; i++) {
     document.getElementById("contents").innerHTML += `
 <div class="box">
   <div class="inner-box" style="background: rgba(255, 255, 255, 0.8);">
+    <hr>
     <div class="title">${project[i].title}</div>
-    <hr />
     <a href="${project[i].link}" target="_blank">
       <div class="image">
         <img
@@ -14,12 +14,15 @@ for (var i = 0; i < project.length; i++) {
           alt="${project[i].title}" />
       </div>
     </a>
-    <hr /> ${project[i].completed}
-    <hr /> ${project[i].details}
+     ${project[i].completed}
+     <hr>
+     <div class="project_details">
+     ${project[i].details}
+     </div>
   </div>
-  <a href="${project[i].github}" target="_blank"
+  <!--<a href="${project[i].github}" target="_blank"
     rel="noopener noreferrer">
     <div class="github-code">Code on Github</div>
-  </a>
+  </a>-->
 </div>`;
 }
